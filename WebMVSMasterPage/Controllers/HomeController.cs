28d6df3b17cef49dd9c591d2290ff24a3e7c9e14
AliMusayevv <emusayev001@gmail.com>
2022-12-20 23:50:36 -0800

    Add project files.

---
+++ WebMVSMasterPage/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVSMasterPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Galery()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ lesson4MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using lesson4MVC.Models.EntityFrameWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lesson4MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities db =new NorthwindEntities();
+        public ActionResult Index()
+        {
+            var model=db.Orders.ToList();
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Yeni()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public ActionResult Yeni(string ad)
+        {
+            return View();
+        }
+    }
+}
